d60a454199cde60ce1cbecf26b27982d3940919d
Rwanda S <rwanda.ace@gmail.com>
2020-01-23 18:19:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,3 +4,16 @@ function updateObjectWithKeyAndValue(obj, key, value){
  var newObj = Object.assign( {}, obj, {[key]:value})
   return newObj
 }
+function destructivelyUpdateObjectWithKeyAndValue(obj, key, value){
+  Object.assign(obj, {[key]:value})
+  return obj
+}
+function deleteFromObjectByKey(obj, key){
+var newObj =  Object.assign({}, obj, {[key]:value}) 
+delete newObj[key]
+return newObj
+}
+function destructivelyDeleteFromObjectByKey(obj,key){
+  delete obj[key]
+  return obj
+}
